7e4821f826a2ea2b1f040ccfa72250fca0f0c6aa
Henrik Niskanen <henrik.niskanen@aalto.fi>
2024-01-12 00:20:52 +0200

    Implement flags, which don't work due to reqwest limitations

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,10 @@ const API_PATH: &str = "https://file.io";
 #[derive(Parser)]
 struct Cli {
     file_path: std::path::PathBuf,
+    #[clap(long, short('d'), action)]
+    no_delete: bool,
+    #[clap(long, short('n'), action)]
+    max_downloads: i32
 }
 #[derive(Debug, Serialize, Deserialize)]
 struct FileRequest {
@@ -27,18 +31,27 @@ async fn main() -> Result<()> {
     let args = Cli::parse();
     let file_path = args.file_path;
     let file_name = String::from(file_path.file_name().unwrap().to_str().unwrap());
-    post_file(file_path, file_name).await?;
+    let res = post_file(file_path, file_name, args.no_delete, args.max_downloads).await;
+    match res {
+        Ok(()) => {}
+        Err(error) => println!("File could not be uploaded\n{}", error.to_string())
+    }
 
     Ok(())
 }
 
-async fn post_file(file_path: std::path::PathBuf, file_name: String ) -> Result<()> {
-    let bytes = std::fs::read(&file_path).unwrap();    //let byte_string = String::from_utf8(bytes).unwrap();
-    
-    let part = reqwest::multipart::Part::bytes(bytes)
+async fn post_file(file_path: std::path::PathBuf, file_name: String, no_delete: bool, max_downloads: i32 ) -> Result<(), Box<dyn std::error::Error>> {
+    let bytes = std::fs::read(&file_path)?;
+    println!("{}", (!no_delete).to_string());
+    println!("{}", max_downloads.to_string());
+    let file_part = reqwest::multipart::Part::bytes(bytes)
         .file_name(file_name);
+
+
     let form = reqwest::multipart::Form::new()
-        .part("file", part);
+        .part("file", file_part);
+        //.text("autoDelete", format!("\"{}\"", (!no_delete).to_string()))
+        //.text("maxDownloads", max_downloads.to_string());
 
     let result_json =  reqwest::Client::new()
         .post(API_PATH)
@@ -47,6 +60,7 @@ async fn post_file(file_path: std::path::PathBuf, file_name: String ) -> Result<
         .await?;
     
     let response = result_json.json::<FileResponse>().await?;
+
     
     // Create clipboardContext
     let mut ctx = ClipboardContext::new().unwrap();
